9ca1d15e078e86779568995c16235c8da6542dca
QueensKid <andres.ibarra0716@gmail.com>
2017-12-20 17:50:53 -0800

    Plant Garden Complete

---
+++ PlantGarden/PlantGarden/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+
+
+namespace PlantGarden
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Char P = 'X';
+            for (int i = 0; i < 5; i++)
+            {
+                for (int n = 0; n < 5; n++)
+                {
+                    Console.Write(P);
+                }
+
+                Console.WriteLine();
+                P = (P == 'X') ? 'Y' : 'X';
+            }
+
+            Console.WriteLine();
+
+            P = 'X';
+            for (int i = 0; i < 5; i++)
+            {
+                for (int n = 0; n < 5; n++)
+                {
+                    Console.Write(P);
+                    P = (P == 'X') ? 'Y' : 'X';
+                }
+                Console.WriteLine();
+
+            }
+
+            Console.ReadLine();
+
+
+        }
+    }
+}
