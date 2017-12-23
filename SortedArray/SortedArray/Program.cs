41e1e3b8382f91cfd20cbb2d0db89e02e8bf9963
QueensKid <andres.ibarra0716@gmail.com>
2017-12-22 17:58:40 -0800

    Sorted Array challenge complete

---
+++ SortedArray/SortedArray/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+namespace SortedArray
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] myArray = new int[] { 4, 4, 4, 4, 8, 15, 15, 16, 16, 16, 16, 23, 23, 42 };
+
+            int counter = 0;
+            int current = 0;
+
+            for (int i = 0; i < myArray.Length; i++)
+            {
+
+                current = myArray[i];
+                while (i < myArray.Length && current == myArray[i])
+                {
+                    counter ++;
+                    i++;
+                }
+                Console.WriteLine(current + " : " + counter);
+                counter = 0;
+                i--;
+
+            }
+            Console.ReadLine();
+           
+        }
+
+
+    }
+}
+
