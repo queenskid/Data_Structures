754c989c966c98d5e8f831cc28fd552ada969cfc
QueensKid <andres.ibarra0716@gmail.com>
2018-01-16 17:25:32 -0800

    Tree Traversal started, need to do the console app for verification

---
+++ TreeTraversal/TreeTraversal/Node.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace TreeTraversal
+{
+    class Node
+    {
+        public int value;
+        public Node left;
+        public Node right;
+        public void display()
+        {
+            Console.Write("(");
+            Console.Write(value);
+            Console.Write(")");
+
+        }
+    }
+}
---
+++ TreeTraversal/TreeTraversal/Tree.cs
@@ -0,0 +1,90 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace TreeTraversal
+{
+    class Tree
+    {
+        public Node root;
+
+        public Tree()
+        {
+            root = null;
+        }
+
+        public Node ReturnRoot()
+        {
+            return root;
+        }
+
+
+        public void Insert(char i)
+        {
+            Node newNode = new Node();
+            newNode.value = i;
+            if (root == null)
+                root = newNode;
+            else
+            {
+                Node current = root;
+                Node parent;
+                while (true)
+                {
+                    parent = current;
+                    if (i < current.value)
+                    {
+                        current = current.left;
+                        if (current == null)
+                        {
+                            parent.left = newNode;
+                            return;
+                        }
+                    }
+                    else
+                    {
+                        current = current.right;
+                        if (current == null)
+                        {
+                            parent.right = newNode;
+                            return;
+                        }
+                    }
+                }
+            }
+        }
+
+        public void Preorder(Node Root)
+        {
+            if (Root != null)
+            {
+                Console.Write(Root.value + " ");
+                Preorder(Root.left);
+                Preorder(Root.right);
+            }
+        }
+
+
+        public void Inorder(Node Root)
+        {
+            if (Root != null)
+            {
+                Inorder(Root.left);
+                Console.Write(Root.value + " ");
+                Inorder(Root.right);
+            }
+        }
+
+
+        public void Postorder(Node Root)
+        {
+            if (Root != null)
+            {
+                Postorder(Root.left);
+                Postorder(Root.right);
+                Console.Write(Root.value + " ");
+            }
+        }
+    }
+}
+
