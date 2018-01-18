c17e29baa56c577721bfa8d00394814766247fe9
QueensKid <andres.ibarra0716@gmail.com>
2018-01-18 13:57:03 -0800

    Question tree scaffolding started

---
+++ QandATree/QandATree/Node.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace QandATree
+{
+    class Node
+    {
+        public string Question { get; set; }
+
+        public string Answer { get; set; }
+
+        public Node Left { get; set; }
+
+        public Node Right { get; set; }
+
+    }
+}
