02d8b6e07a0231e05f7967ad9e66a1226636c902
QueensKid <andres.ibarra0716@gmail.com>
2018-01-30 17:44:21 -0800

    Properties created

---
+++ ReplaceNodes/ReplaceNodes/Node.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ReplaceNodes
+{
+    class Node
+    {
+        public Node Root { get; set; }
+        public object Value { get; set; }
+        public Node Left { get; set; }
+        public Node Right { get; set; }
+
+        public Node(object data)
+        {
+            Value = data;
+        }
+    }
+}
---
+++ ReplaceNodes/ReplaceNodes/NodeSwap.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ReplaceNodes
+{
+    class NodeSwap
+    {
+        Node Root = new Node();
+
+        public void Swap(int one, int two)
+        {
+            Queue<Node> q = new Queue<Node>();
+
+            Node swapNode1;
+            Node swapNode2;
+            q.Enqueue(Root);
+        }
+    }
+}
